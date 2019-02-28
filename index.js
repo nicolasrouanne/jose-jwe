f396a910e1d2ec02296a3f3e94b84ea1ef68ebae
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-02-28 20:58:04 +0100

    Extract an encrypt function returning a Promise

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const keyStr = require("./keyStr");
 const jose = require("node-jose");
 
 const keystore = jose.JWK.createKeyStore();
-keystore.add(keyStr).then(key => {
-  console.log(key);
 
-  jose.JWE.createEncrypt({ format: "compact" }, key)
-    .update("My Encrypted message")
-    .final()
-    .then(enc => {
-      console.log(enc);
-    });
-});
+const encrypt = input =>
+  keystore.add(keyStr).then(key =>
+    jose.JWE.createEncrypt({ format: "compact" }, key)
+      .update(input)
+      .final()
+  );
+
+encrypt("foo").then(enc => console.log(enc));
