60d52e0716987d3f3e629e766be8b0d9e2d8cd79
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-03-15 16:32:00 +0100

    feat(format): add display in flattened (JSON) format
    - @link https://github.com/cisco/node-jose/tree/2595b4ad41018cd81a97d88192e2de2bd9db1b5b#encrypting-content

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ const encryptHashValues = hash => {
   return Promise.all(encryptEntries).then(() => encryptedHash);
 };
 
-const encrypt = input =>
+const encrypt = (input, format = "compact") =>
   jose.JWK.asKey(keyStr).then(key =>
-    jose.JWE.createEncrypt({ format: "compact" }, key)
+    jose.JWE.createEncrypt({ format }, key)
       .update(input)
       .final()
   );
 
-encrypt("My Encrypted message").then(enc => console.log(enc));
+encrypt("My Encrypted message").then(enc => console.log(`Compact:\n${enc}\n`));
+encrypt("My Encrypted message", "flattened").then(enc => {
+  console.log("JSON:");
+  console.log(enc);
+  console.log("\n");
+});
 encryptHashValues(payload).then(encryptedHash => console.log(encryptedHash));
