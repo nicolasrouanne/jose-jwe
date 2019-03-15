70598a5d06fb7689fa1154518753600ed315e924
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-03-15 16:47:57 +0100

    feat(thumbprint): add thumbrint in key header
    - @link https://github.com/cisco/node-jose/tree/2595b4ad41018cd81a97d88192e2de2bd9db1b5b#obtaining-a-keys-thumbprint

--- encrypt.js
+++ encrypt.js
@@ -3,9 +3,21 @@ const jose = require("node-jose");
 
 const encrypt = (input, format = "compact") =>
   jose.JWK.asKey(keyStr).then(key =>
-    jose.JWE.createEncrypt({ format }, key)
-      .update(input)
-      .final()
+    key.thumbprint().then(thumbprint =>
+      jose.JWE.createEncrypt(
+        {
+          fields: {
+            alg: "RSA-OAEP",
+            enc: "A256GCM",
+            kid: thumbprint
+          },
+          format
+        },
+        key
+      )
+        .update(input)
+        .final()
+    )
   );
 
 module.exports = encrypt;
